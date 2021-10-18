405ab22d9d5232c4f59f685f51dc08bcf373a4ce
TomislavHranicNeuralab <tomislav@neuralab.net>
2021-10-18 15:37:57 +0200

    Get elements

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Get our elemnts
+const player      = document.querySelector('.player');
+const video       = player.querySelector('.viewer');
+const controls    = player.querySelector('.player__controls');
+const progress    = controls.querySelector('.progress');
+const progressBar = progress.querySelector('.progress__filled');
+const toggle      = controls.querySelector('.toggle');
+const skipButtons = controls.querySelectorAll('[data-skip]');
+const ranges      = controls.querySelectorAll('.player__slider');
+
+// Build functions
+
+// Hook up the event listeners
